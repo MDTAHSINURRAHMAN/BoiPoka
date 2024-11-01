05157ae7594fefa321961a5c30cbdf5ea73422fa
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-11-01 17:12:28 +0600

    Load Book Details Data And Show == vs ===

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,10 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetail = () => {
     const {bookId} = useParams();
+    const id = parseInt(bookId);
+    const data = useLoaderData();
+    const book = data.find(book => book.bookId === id);
+    const {bookId: currentBookId, image} = book;
     return (
-        <div>
+        <div className="my-12">
             <h2>Book Details: {bookId}</h2>
+            <img className="w-36" src={image} alt="" />
+            <br />
+            <button className="btn btn-outline mr-4 btn-accent">Read</button>
+            <button className="btn btn-accent">Wishlist</button>
         </div>
     );
 };
