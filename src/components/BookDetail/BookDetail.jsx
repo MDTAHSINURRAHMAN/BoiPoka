c70a27ce42da9bb9bd895cee577ab28224b53467
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-11-01 17:27:35 +0600

    Save Mark As Read To Somewhere

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../utility/addToDB";
 
 const BookDetail = () => {
     const {bookId} = useParams();
@@ -6,13 +7,18 @@ const BookDetail = () => {
     const data = useLoaderData();
     const book = data.find(book => book.bookId === id);
     const {bookId: currentBookId, image} = book;
+
+    const handleMarkAsRead = (id) => {
+        addToStoredReadList(id);
+    }
+
     return (
         <div className="my-12">
             <h2>Book Details: {bookId}</h2>
             <img className="w-36" src={image} alt="" />
             <br />
-            <button className="btn btn-outline mr-4 btn-accent">Read</button>
-            <button className="btn btn-accent">Wishlist</button>
+            <button onClick={() => handleMarkAsRead(bookId)} className="btn btn-outline mr-4 btn-accent">Mark As Read</button>
+            <button className="btn btn-accent">Add To Wishlist</button>
         </div>
     );
 };
