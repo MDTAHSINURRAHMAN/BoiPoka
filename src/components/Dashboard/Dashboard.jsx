f3058a4f28eaec0dac27e48437689cbbe7807043
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-11-01 15:36:51 +0600

    Nested Route And Navbar Setup

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">Dhisum Dhisum Dashing Board</h2>
+        </div>
+    );
+};
+
+export default Dashboard;
